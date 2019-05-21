280bc42077571e0cad726a899a3353ff5f900777
kepler90 <contact@rodrigoesteves.fr>
2019-05-21 17:12:08 +0200

    Init front

---
+++ public/wip/include_footer.php
@@ -0,0 +1,3 @@
+<footer id="footer">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dolores, illum itaque perspiciatis corporis enim aliquam ab nesciunt corrupti nihil natus explicabo blanditiis quasi sed molestias. Accusamus animi quo cupiditate.
+</footer>
