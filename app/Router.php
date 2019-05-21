057384c76335dbdadc3e3e20b30dc126cc46ee29
Roupsard <david.roupsard@gmail.com>
2019-05-21 16:53:04 +0200

    MAJ Router.php & index.php + .htaccess

--- app/Router.php
+++ app/Router.php
@@ -4,6 +4,16 @@ namespace app;
 
 class Router {
 
+    public function __construct($url) {
+        if ($url === '/') {
+            $index = new IndexController();
+        } else {
+            $requestController  = ucfirst($url[0]);
+            $requestAction      = isset($url[1]) ? $url[1] : '';
+            $requestParam       = array_slice($url, 2);
 
+            $ctrlPath = 'controller/'.$requestController.'.php';
+        }
+    }
 
 }
--- public/index.php
+++ public/index.php
@@ -4,5 +4,5 @@ use app\autoload;
 require '../app/Autoload.php';
 
 Autoload::register();
-
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : '/';
 $router = new Router();
