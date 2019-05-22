3c5d5cf1ea75c2145f2a3e16f4ee3b1523dae53a
JBA <jbacorrtech@gmail.com>
2019-05-22 16:59:23 +0200

    remplacement user.php par profil.php

--- app/views/templates/user/profil.php
+++ app/views/templates/user/profil.php
@@ -1,4 +1,4 @@
-<?php include_once('./include_head.php');?>
+
         <section id="infos" class="container-fluid">
             <div class="row">
                 <img class="col-3" id="avatar" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2dadf83d-672b-4fee-91ec-38b215d60224/d2uuk4s-941fe923-9c4f-4632-b23d-7fc925d0254b.jpg/v1/fill/w_811,h_985,q_70,strp/chocobo_by_aikaxx_d2uuk4s-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA5MiIsInBhdGgiOiJcL2ZcLzJkYWRmODNkLTY3MmItNGZlZS05MWVjLTM4YjIxNWQ2MDIyNFwvZDJ1dWs0cy05NDFmZTkyMy05YzRmLTQ2MzItYjIzZC03ZmM5MjVkMDI1NGIuanBnIiwid2lkdGgiOiI8PTkwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.acRbEIMvTacpilzRfPUeOTjZn6jsny40C4o8JPxb_FE" alt="avatar" />
