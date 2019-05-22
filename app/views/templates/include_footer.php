b973c41dedb5186d7bfca451757105c6e756c4b9
Roupsard <david.roupsard@gmail.com>
2019-05-22 13:56:21 +0200

    Copie des fichiers vues dans le dossier template

---
+++ app/views/templates/include_footer.php
@@ -0,0 +1,16 @@
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Footer 1</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="col-md-6">
+                <h3>Footer 2</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>. 
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
