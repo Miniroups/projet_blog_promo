2443843bc46135f33f319a88f4f3b22b4ae377f1
root <kazarinov.yanek@gmail.com>
2019-05-22 14:25:45 +0200

    video

---
+++ public/wip/home.php
@@ -0,0 +1,16 @@
+<?php include_once('./include_head.php');?>
+<?php include_once('./include_header.php');?>
+
+<div class="content">
+
+    <div class='container-fluid nopadding'>
+        <div class="video">
+            <iframe  src="https://www.youtube.com/embed/b4u3t9gUrmQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        123
+    </div>
+
+   
+</div> <!--content-->
+
+<?php include_once('./include_footer.php');?>
