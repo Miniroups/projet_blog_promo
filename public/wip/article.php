fbb06e2e3895f811b9ed6d52638d92370e4e4445
kepler90 <contact@rodrigoesteves.fr>
2019-05-22 13:54:33 +0200

    Initialisation de la branche
    Problème de compatibilité.

--- public/wip/article.php
+++ public/wip/article.php
@@ -5,18 +5,42 @@
 
     <!-- En tête de l'article -->
     <div class="container-fluid head-article">
+        <img class="background" src="http://placehold.it/1980x1180" alt="Image de l'article">
         <h1>Titre de mon Article</h1>
     </div>
 
     <!-- Body de l'article -->
     <section class="container article">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque a sed magni. Minima voluptas, est odit ipsa id rerum inventore ex laudantium nostrum beatae eius labore amet excepturi voluptates esse.
+        <article>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi temporibus repellendus, assumenda consequuntur cum, doloremque magnam architecto tempora vel placeat ad obcaecati, repellat consequatur ea. Voluptas debitis libero ducimus praesentium!
+        </article>
     </section>
 
     <!-- Espace commentaire de l'article -->
     <section class="container commentaire">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quae iure eligendi! Quidem, dolorum, nesciunt rerum quas explicabo maiores saepe corporis corrupti laboriosam nihil sint! Nesciunt mollitia ipsum eaque neque!
+
+        <!-- Ajouter un nouveau commentaire -->
+        <div class="new-comment">
+            <form id="form-commentaire" name="form-commentaire" action="#" method="#">
+                <div class="form-group">
+                    <label for="name">Nom</label>
+                    <input type="text" class="form-control" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="content">Commentaire</label>
+                    <textarea id="content" name="content" cols="2" class="form-control"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary" name="submit-commentaire">Envoyer mon commentaire</button>
+            </form>
+        </div>
+
+        <!-- Afficher les commentaires -->
+        <div class="show-comments">
+
+        </div>
+
     </section>
+
 </div>
 
 <?php include_once('./include_footer.php');?>
