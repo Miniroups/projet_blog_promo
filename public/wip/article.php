b1ddb32c1f5f32d63d9346f2f4e4fb89a7c7acca
kepler90 <contact@rodrigoesteves.fr>
2019-05-22 11:59:58 +0200

    Update for Yanek v2

--- public/wip/article.php
+++ public/wip/article.php
@@ -1,6 +1,8 @@
 <?php include_once('./include_head.php');?>
-    
 <?php include_once('./include_header.php');?>
+
+<div class="content article-show">
+
     <!-- En tête de l'article -->
     <div class="container-fluid head-article">
         <h1>Titre de mon Article</h1>
@@ -10,5 +12,11 @@
     <section class="container article">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque a sed magni. Minima voluptas, est odit ipsa id rerum inventore ex laudantium nostrum beatae eius labore amet excepturi voluptates esse.
     </section>
+
+    <!-- Espace commentaire de l'article -->
+    <section class="container commentaire">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quae iure eligendi! Quidem, dolorum, nesciunt rerum quas explicabo maiores saepe corporis corrupti laboriosam nihil sint! Nesciunt mollitia ipsum eaque neque!
+    </section>
+</div>
 
 <?php include_once('./include_footer.php');?>
