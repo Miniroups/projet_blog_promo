bba84d5f45a32153cb8c2dfc2aed0b51560a9299
kepler90 <contact@rodrigoesteves.fr>
2019-05-22 11:52:33 +0200

    Update for Yanek

---
+++ public/wip/article.php
@@ -0,0 +1,14 @@
+<?php include_once('./include_head.php');?>
+    
+<?php include_once('./include_header.php');?>
+    <!-- En tête de l'article -->
+    <div class="container-fluid head-article">
+        <h1>Titre de mon Article</h1>
+    </div>
+
+    <!-- Body de l'article -->
+    <section class="container article">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque a sed magni. Minima voluptas, est odit ipsa id rerum inventore ex laudantium nostrum beatae eius labore amet excepturi voluptates esse.
+    </section>
+
+<?php include_once('./include_footer.php');?>
