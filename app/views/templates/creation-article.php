fdc47342f9a3f96e9c939a7d10ef14eff1569815
Roupsard <david.roupsard@gmail.com>
2019-05-22 15:22:24 +0200

    Ajout du creation-article.php et son css

---
+++ app/views/templates/creation-article.php
@@ -0,0 +1,26 @@
+<body>
+
+    <h1>Création de l'article</h1>
+    <div id="creationArticle" class="container">
+        <div class="formInput form-group">
+            <label for="titreArticle">Titre de l'article</label>
+            <input type="text" placeholder="Le titre de votre article ...">
+        </div>
+        <div class="row">
+            <div class="col formInput form-group">
+                <label for="imageArticle">Image de l'article</label>
+                <input type="text" placeholder="L'image de votre article ...">
+            </div>
+            <div class="col formInput form-group">
+                <label for="altImgArticle">Alternative de l'image</label>
+                <input type="text" placeholder="L'alternative de votre image ...">
+            </div>
+        </div>
+        <div class="formInput form-group">
+            <label for="texteArticle">Texte de votre article</label>
+            <textarea placeholder="Entrez le texte de votre article ici ..." rows="10"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Je valide mon article !</button>
+    </div>
+
+</body>
