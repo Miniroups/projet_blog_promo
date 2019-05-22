f0b74ba6d09baa4a5eed99c7cca1c0412cf235bb
Roupsard <david.roupsard@gmail.com>
2019-05-22 15:56:58 +0200

    Ajout de inscription.php + CSS + modif name creation-article.php

--- app/views/templates/blog/creation-article.php
+++ app/views/templates/blog/creation-article.php
@@ -4,21 +4,21 @@
     <div id="creationArticle" class="container">
         <div class="formInput form-group">
             <label for="titreArticle">Titre de l'article</label>
-            <input type="text" placeholder="Le titre de votre article ...">
+            <input type="text" placeholder="Le titre de votre article ..." name="titreArticle">
         </div>
         <div class="row">
             <div class="col formInput form-group">
                 <label for="imageArticle">Image de l'article</label>
-                <input type="text" placeholder="L'image de votre article ...">
+                <input type="text" placeholder="L'image de votre article ..." name="imageArticle">
             </div>
             <div class="col formInput form-group">
                 <label for="altImgArticle">Alternative de l'image</label>
-                <input type="text" placeholder="L'alternative de votre image ...">
+                <input type="text" placeholder="L'alternative de votre image ..." name="altImgArticle">
             </div>
         </div>
         <div class="formInput form-group">
             <label for="texteArticle">Texte de votre article</label>
-            <textarea placeholder="Entrez le texte de votre article ici ..." rows="10"></textarea>
+            <textarea placeholder="Entrez le texte de votre article ici ..." rows="10" name="texteArticle"></textarea>
         </div>
         <button type="submit" class="btn btn-success">Je valide mon article !</button>
     </div>
---
+++ app/views/templates/user/inscription.php
@@ -0,0 +1,46 @@
+<body>
+
+    <h1>Inscription / Edition de votre profil</h1>
+    <section id="inscription" class="container">
+        <div class="row">
+            <div class="col formInput form-group">
+                <label for="identifiant">identifiant</label>
+                <input type="text" placeholder="Votre identifiant ..." name="identifiant">
+            </div>
+            <div class="col formInput form-group">
+                <label for="email">Email</label>
+                <input type="text" placeholder="Votre adresse email ..." name="email">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col formInput form-group">
+                <label for="password">Mot de passe</label>
+                <input type="text" placeholder="Votre mot de passe ..." name="password">
+            </div>
+            <div class="col formInput form-group">
+                <label for="confirmPassword">Confirmation mot de passe</label>
+                <input type="text" placeholder="Confirmaton de votre mot de passe ..." name="password">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col formInput form-group">
+                <label for="name">Nom</label>
+                <input type="text" placeholder="Votre nom ..." name="name">
+            </div>
+            <div class="col formInput form-group">
+                <label for="firstname">Prénom</label>
+                <input type="text" placeholder="Votre prénom ..." name="firstname">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col formInput form-group">
+                <label for="imageProfil">Image de profil</label>
+                <input type="text" placeholder="L'image de votre profil ..." name="imageProfil">
+            </div>
+            <div class="col formInput form-group" id="jambon">
+                <button type="submit" class="btn btn-success">Je valide mon inscription !</button>
+            </div>
+        </div>
+    </section>
+
+</body>
