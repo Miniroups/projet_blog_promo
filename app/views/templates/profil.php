e8e3c3d2eeb4a44bea0e59a68ed0fd8f30d60bed
JBA <jbacorrtech@gmail.com>
2019-05-22 16:31:13 +0200

    Vue profil user

---
+++ app/views/templates/profil.php
@@ -0,0 +1,19 @@
+<?php include_once('./include_head.php');?>
+        <section id="infos" class="container-fluid">
+            <div class="row">
+                <img class="col-3" id="avatar" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2dadf83d-672b-4fee-91ec-38b215d60224/d2uuk4s-941fe923-9c4f-4632-b23d-7fc925d0254b.jpg/v1/fill/w_811,h_985,q_70,strp/chocobo_by_aikaxx_d2uuk4s-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA5MiIsInBhdGgiOiJcL2ZcLzJkYWRmODNkLTY3MmItNGZlZS05MWVjLTM4YjIxNWQ2MDIyNFwvZDJ1dWs0cy05NDFmZTkyMy05YzRmLTQ2MzItYjIzZC03ZmM5MjVkMDI1NGIuanBnIiwid2lkdGgiOiI8PTkwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.acRbEIMvTacpilzRfPUeOTjZn6jsny40C4o8JPxb_FE" alt="avatar" />
+                <div class="col-9">
+                    <div class="row justify-content-around">
+                        <h3>Nom : Machin</h3>
+                        <h3>Prénom : Truc</h3><br>
+                    </div>
+                    <h3>Email : adresse.gmail.com</h3><br>
+                    <a href="#">Modifier mes informations</a>
+                </div>    
+            </div><br>
+            <h3>Mes articles :</h3><br>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus assumenda, aliquam distinctio architecto saepe deserunt. Sequi, quaerat totam minus veniam dolores assumenda tempora ipsa doloremque ullam eum repellendus tenetur! Sit?</p><br>
+            <button type="button" class="btn btn-light">Charger plus d'articles</button>
+        </section>
+    </body>
+</html>    
