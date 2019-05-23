fce7607923878965c9102db19845a043a741d75f
kepler90 <contact@rodrigoesteves.fr>
2019-05-23 13:59:54 +0200

    Ajout page connexion wip

---
+++ public/wip/connexion.php
@@ -0,0 +1,23 @@
+<?php include_once('./include_head.php');?>
+
+<div class="panel-login">
+    
+    <div class="zone">
+        <h1>Espace de connexion</h1>
+        <div class="login">
+            <form method="" action="">
+                <div class="form-group">
+                    <label for="login"><i class="fas fa-user-lock"></i> Indentifiant</label>
+                    <input type="email" class="form-control" id="login">
+                </div>
+                <div class="form-group">
+                    <label for="password"><i class="fas fa-key"></i> Mot de passe</label>
+                    <input type="password" class="form-control" id="password">
+                </div>
+                <button type="submit" class="btn btn-success form-control">Se connecter</button>
+            </form>
+            <a class="register" href="#"><i class="fas fa-chevron-right"></i> S'inscrire</a>  
+        </div>
+    </div>
+
+</div>
