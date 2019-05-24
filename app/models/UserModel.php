d2294f5bf8b28147e24196fed1747d73a3ceb422
Jarod <jarod.midy@gmail.com>
2019-05-24 11:34:07 +0200

    inscription et edit de profil qui fonctionne normalment

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -22,11 +22,12 @@ class UserModel extends Model
 
   public function addUser(array $userData)
   {
-
+    $this->getData('INSERT INTO users (id, firstname, lastname, email, login, password, url_avatar, file, about) VALUES (NULL, :firstname, :name, :email, :identifiant, :password, :imageProfil, :cvUpload, :description)',$userData);
   }
 
   public function updateUser(array $userData)
   {
-    // code...
+    array_push($userData, ['id'=> $_SESSION['userInfos']['id']]);
+    $this->getData('UPDATE users SET firstname = :firstname, lastname = :name,email = :email,login = :identifiant, password = :password, url_avatar = :imageProfil, file = :cvUpload, about = :description WHERE id = :id',$userData);
   }
 }
