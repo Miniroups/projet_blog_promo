a0da9267a9bd45f3cd76406a0b5b082d1c86203b
JBA <jbacorrtech@gmail.com>
2019-05-24 11:23:54 +0200

    Ajout lien accueil au logo nav

--- app/views/templates/parts/nav.php
+++ app/views/templates/parts/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-    <a class="navbar-brand large" href="#"><img class='tree' src="<?= $publicPath ?>/img/logo_tree.png"><img class="text" src="<?= $publicPath ?>/img/logo_text.png"></a>
+    <a class="navbar-brand large" href="<?= $publicPath?>/home"><img class='tree' src="<?= $publicPath ?>/img/logo_tree.png"><img class="text" src="<?= $publicPath ?>/img/logo_text.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
